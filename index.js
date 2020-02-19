87581749f1b1669e0cec1042f221e98b05411d6f
Josh Stratton <strattonbrazil@gmail.com>
2020-02-18 21:08:22 -0800

    move to express

--- index.js
+++ index.js
@@ -1,25 +1,14 @@
-// using the http module
-let http = require('http'),
- 
-// look for PORT environment variable, 
-// else look for CLI argument,
-// else use hard coded value for port 8080
-port = process.env.PORT || process.argv[2] || 8080;
- 
-// create a simple server
-let server = http.createServer(function (req, res) {
- 
-        res.writeHead(200, {
-            'Content-Type': 'text/plain'
-        });
-        res.write('hello heroku!', 'utf-8');
-        res.end();
- 
-    });
- 
-// listen on the port
-server.listen(port, function () {
- 
-    console.log('app up on port: ' + port);
- 
-});
+const express = require('express')
+const app = express()
+const port = process.env.PORT || process.argv[2] || 8080
+
+app.get('/', (req, res) => {
+    let proto = "http";
+    if (req.headers["X-Forwarded-Proto"]) {
+        proto = req.headers["X-Forwarded-Proto"];
+    }
+    res.send('Hello World! from: ' + proto);
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
