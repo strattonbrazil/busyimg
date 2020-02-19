7035b43edc56d8b78924bdcd0794455c8dd3d3c4
Josh Stratton <strattonbrazil@gmail.com>
2020-02-18 21:18:56 -0800

    using header

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const app = express()
 const port = process.env.PORT || process.argv[2] || 8080
 
 app.get('/', (req, res) => {
-    const proto = req.protocol;
-    res.send('Hello World! from: ' + JSON.stringify(req.headers));
+    const proto = req.get("x-forwarded-proto") ? req.get("x-forwarded-proto") : "http";
+    res.send('Hello World! from: ' + proto);
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
