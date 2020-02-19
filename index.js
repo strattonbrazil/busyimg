0c609ec4f58b8c34c3d368595fb7fc46d2f406c6
Josh Stratton <strattonbrazil@gmail.com>
2020-02-18 20:29:14 -0800

    first step

---
+++ index.js
@@ -0,0 +1,25 @@
+// using the http module
+let http = require('http'),
+ 
+// look for PORT environment variable, 
+// else look for CLI argument,
+// else use hard coded value for port 8080
+port = process.env.PORT || process.argv[2] || 8080;
+ 
+// create a simple server
+let server = http.createServer(function (req, res) {
+ 
+        res.writeHead(200, {
+            'Content-Type': 'text/plain'
+        });
+        res.write('hello heroku!', 'utf-8');
+        res.end();
+ 
+    });
+ 
+// listen on the port
+server.listen(port, function () {
+ 
+    console.log('app up on port: ' + port);
+ 
+});
