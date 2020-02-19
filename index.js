df0f40847975837cf7927db92cadab63b28c4c34
Josh Stratton <strattonbrazil@gmail.com>
2020-02-18 21:45:23 -0800

    add static and ejs

--- index.js
+++ index.js
@@ -2,10 +2,26 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || process.argv[2] || 8080
 
+app.set('view engine', 'ejs');
+
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+        console.log("redireting to https");
+      return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
+
+app.use(requireHTTPS);
+
+app.use("/static", express.static('static'));
+
 app.get('/', (req, res) => {
-    const proto = req.get("x-forwarded-proto") ? req.get("x-forwarded-proto") : "http";
-    res.send('Hello World! from: ' + proto);
+    res.render('pages/index');
 })
+
+console.log(process.env.NODE_ENV);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
