c744e5e027d535140b8d009e6c988ca7b4898236
Josh Stratton <strattonbrazil@gmail.com>
2020-02-19 22:56:57 -0800

    got Google OAuth working

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 ### Running
 
+Get the Google credentials [here](https://console.developers.google.com/apis/credentials/oauthclient/730090819540-h8r35h0rger50psc5o2oiksafmnp7erc.apps.googleusercontent.com?project=busyimg-1582090529531).  
+
 ```
+export GOOGLE_CLIENT_ID="..."
+export GOOGLE_CLIENT_SECRET="..."
 NODE_ENV=development npx nodemon index.js
 ```
--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 const express = require('express')
+const passport = require('passport')
+const auth = require('./auth')
+const cookieParser = require('cookie-parser')
+const cookieSession = require('cookie-session')
+
 const app = express()
+
+auth(passport);
+app.use(passport.initialize());
+
+
+
+if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
+    console.error("no Google auth envs located");
+    process.exit(1);
+}
+
 const port = process.env.PORT || process.argv[2] || 8080
 
 app.set('view engine', 'ejs');
@@ -17,7 +33,43 @@ app.use(requireHTTPS);
 
 app.use("/static", express.static('static'));
 
+app.use(cookieSession({
+    name: 'session',
+    keys: ['AHuXy5kB4w1iMxzRBrctNTo6oP1u9mDT']
+}));
+
+app.use(cookieParser());
+
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['https://www.googleapis.com/auth/userinfo.profile']
+}));
+
+app.get('/auth/google/callback',
+    passport.authenticate('google', {
+        failureRedirect: '/'
+    }),
+    (req, res) => {
+        console.log(JSON.stringify(req.user));
+        req.session.token = req.user.token;
+        res.redirect('/');
+    }
+);
+
 app.get('/', (req, res) => {
+    if (req.session.token) {
+        console.log("valid token");
+        //res.cookie('token', req.session.token);
+        //res.json({
+        //   status: 'session cookie set'
+        //});
+    } else {
+        //console.log("no valid token");
+        //res.cookie('token', '')
+        //res.json({
+        //    status: 'session cookie not set'
+        //});
+    }
+
     res.render('pages/index');
 })
 
