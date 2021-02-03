720c929d15215ec802b19ae193aa16b950aff559
Josh Stratton <strattonbrazil@gmail.com>
2021-02-02 22:50:16 -0800

    add route for direct requests to subpaths

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ var express = require('express');
 var app = express();
 // Render static files
 app.use(express.static('build'));
+
+app.get('/i/[a-zA-z0-9\-]+', function (req, res) {
+    res.sendFile(__dirname + '/build/index.html');
+});
+
 // Port website will run on
 console.log("starting server on port: " + process.env.PORT);
 app.listen(process.env.PORT);
