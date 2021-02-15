919368d19527fd8cebe2b21bee4f2d3832bdf412
jared stratton <jared.stratton@student.centralia.edu>
2021-02-15 10:52:29 -0800

    add and pregen warp-core-cafe

---
+++ src/autogenerated/WarpCoreCafe.ts
@@ -0,0 +1,13 @@
+
+import Metadata from '../Metadata'
+
+export default {
+    "title": "Another Night at the Warp Core Cafe",
+    "subpath": "warp-core-cafe",
+    "creator": "Jeff Carlisle",
+    "creatorLink": "http://jeffcarlisle.com/node/514",
+    "areas": [
+
+    ]
+
+} as Metadata;
